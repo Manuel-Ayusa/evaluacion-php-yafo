4d54d60511b32c53fe5879225b97917d831b648c
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-02-18 22:42:44 -0300

    Solucionar problemas de estilos bootstrap

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CMDBController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('categorias', [CategoryController::class, 'index'])->name('categorias.index');
+Route::get('/', [CategoryController::class, 'index'])->name('categorias.index');
 Route::get('categoria/{id}', [CMDBController::class, 'categorias'])->name('cmdb.categoria');
 Route::get('CMDB/exportar/{id}', [CMDBController::class, 'export'])->name('cmdb.export');
 Route::get('CMDB/create', [CMDBController::class, 'create'])->name('cmdb.create');
