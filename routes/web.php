c9a4c5f9dd5da386c6db3729638c8fa94885fb5d
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-02-19 14:08:43 -0300

    Cambiar rutas a post

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\CMDBController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CategoryController::class, 'index'])->name('categorias.index');
-Route::get('categoria/{id}', [CMDBController::class, 'categorias'])->name('cmdb.categoria');
-Route::get('CMDB/exportar/{id}', [CMDBController::class, 'export'])->name('cmdb.export');
+Route::post('categoria/{id}', [CMDBController::class, 'categorias'])->name('cmdb.categoria');
+Route::post('CMDB/exportar/{id}', [CMDBController::class, 'export'])->name('cmdb.export');
 Route::get('CMDB/create', [CMDBController::class, 'create'])->name('cmdb.create');
 Route::post('CMDB/importar/', [CMDBController::class, 'import'])->name('cmdb.import');
