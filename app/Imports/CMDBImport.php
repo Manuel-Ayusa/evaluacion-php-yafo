f4cb6d39ba37900cdc3e52f18271ead29172f38d
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-02-19 14:05:06 -0300

    Agregar reglas de validación y mejorar array data

--- app/Imports/CMDBImport.php
+++ app/Imports/CMDBImport.php
@@ -5,21 +5,40 @@ namespace App\Imports;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class CMDBImport implements ToCollection
+class CMDBImport implements ToCollection, WithValidation, WithHeadingRow
 {
     /**
     * @param Collection $collection
     */
     public function collection(Collection $collection)
-    {
-        foreach ($collection as $key => $row) {
-            Http::asForm()->post(config('services.api-aleph.url_entorno') . '/API/insert_cmdb', [
-                'api_key' => config('services.api-aleph.api_key'),
-                'identificador' => $row[0], 
-                'nombre' => $row[1],
-                'categoria_id' => $row[4],
-            ]);
+    {   
+        // Si no hay filas o todas están vacías, lanzar una excepción
+        if ($collection->isEmpty() || $collection->isSheetEmpty()) {
+            throw new \Exception('El archivo Excel está vacío.');
         }
+
+        foreach ($collection as $key => $row) {
+
+            $data['api_key'] = config('services.api-aleph.api_key'); //se inicializa el array con 'api_key' para la petición a la API
+
+            foreach ($row as $key => $value) {
+
+                $data[$key] = $value; //se agregan los valores con sus respectivas llaves
+
+            }
+
+            Http::asForm()->post(config('services.api-aleph.url_entorno') . '/API/insert_cmdb', $data); //se manda la petición a la API para que los registros se guarden o actualicen
+        }
+    }
+
+    public function rules(): array
+    {
+        return [
+            'identificador' => 'required',
+            'nombre' => 'required',
+        ];
     }
 }
