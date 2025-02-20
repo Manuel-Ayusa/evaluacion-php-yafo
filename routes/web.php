7b046efb7d4f666f282c6d9ff197f896af486fb1
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-02-19 21:19:39 -0300

    Quitar parametros en rutas posts

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\CMDBController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CategoryController::class, 'index'])->name('categorias.index');
-Route::post('categoria/{id}', [CMDBController::class, 'categorias'])->name('cmdb.categoria');
-Route::post('CMDB/exportar/{id}', [CMDBController::class, 'export'])->name('cmdb.export');
-Route::get('CMDB/create', [CMDBController::class, 'create'])->name('cmdb.create');
+Route::post('categoria/', [CMDBController::class, 'categorias'])->name('cmdb.categoria');
+Route::post('CMDB/exportar/', [CMDBController::class, 'export'])->name('cmdb.export');
+Route::get('CMDB/create/', [CMDBController::class, 'create'])->name('cmdb.create');
 Route::post('CMDB/importar/', [CMDBController::class, 'import'])->name('cmdb.import');
